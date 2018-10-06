fa3b9ca7509e5ea8146da4df8e6e1ca71b5fb306
Zachery Gentry <zacherygentry@gmail.com>
2018-10-06 14:34:04 -0500

    update

---
+++ hackuta-fall-2018/Assets/Scripts/Networking.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Networking : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
