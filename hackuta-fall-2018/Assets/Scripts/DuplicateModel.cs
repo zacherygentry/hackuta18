577db017316ef10195c412b7c4741566d1610888
Christian Gentry <christianjgentry@gmail.com>
2018-10-07 04:30:08 -0500

    Dupe vehicles

--- hackuta-fall-2018/Assets/Scripts/DuplicateModel.cs
+++ hackuta-fall-2018/Assets/Scripts/DuplicateModel.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using VRTK;
 
 public class DuplicateModel : MonoBehaviour {
 
-	void Start () {
-		
-	}
+    public GameObject prefab;
+    public float scale = 2f;
+    bool isDuped = false;
+    void Start()
+    {
+        GetComponent<VRTK_InteractableObject>().InteractableObjectGrabbed += new InteractableObjectEventHandler(ObjectGrabbed);
+        GetComponent<VRTK_InteractableObject>().InteractableObjectUngrabbed += new InteractableObjectEventHandler(ObjectUngrabbed);
+
+    }
+
+    private void ObjectGrabbed(object sender, InteractableObjectEventArgs e)
+    {
+        if (!isDuped)
+        {
+            Vector3 pos = transform.position;
+            GameObject go = Instantiate<GameObject>(prefab, pos, transform.rotation);
+            go.GetComponent<VRTK_InteractableObject>().isGrabbable = true;
+            //go.transform.localScale *= 2f;
+            GetComponent<Transform>().localScale *= scale;
+            isDuped = true;
+        }
+    }
+
+    private void ObjectUngrabbed(object sender, InteractableObjectEventArgs e)
+    {
+       // transform.parent = null;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
-   
-    public void Duplicate(GameObject prefab){
-        GameObject gameObject = Instantiate(prefab, transform);
-    }
 }
