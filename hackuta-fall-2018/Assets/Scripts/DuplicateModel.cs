68cd4e6d783d9742ab209e05e8fb8a7a01fb747b
Zachery Gentry <zacherygentry@gmail.com>
2018-10-07 02:13:13 -0500

    truck dupe

---
+++ hackuta-fall-2018/Assets/Scripts/DuplicateModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DuplicateModel : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+   
+    public void Duplicate(GameObject prefab){
+        GameObject gameObject = Instantiate(prefab, transform);
+    }
+}
